f22eba4639f64e2edebbf0f24fc55dfb50aeccc9
Gameguru <madhurgupt10@gmail.com>
2026-02-14 14:19:57 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Safety polyfill removed as API key availability is handled externally according to guidelines
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
